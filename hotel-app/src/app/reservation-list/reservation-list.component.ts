105a51a8f1095e1acac47b855f94bf9998bb7a58
Anubhaw-Singh <vkt94594@gmail.com>
2025-08-23 23:16:53 +0530

    Deleting reservations

--- hotel-app/src/app/reservation-list/reservation-list.component.ts
+++ hotel-app/src/app/reservation-list/reservation-list.component.ts
@@ -12,11 +12,14 @@ export class ReservationListComponent implements OnInit {
 
 
   reservations: Reservation[] = [];
-  
+
   constructor(private reservationService: ReservationService) { }
 
   ngOnInit(): void {
     this.reservations = this.reservationService.getReservations();
   }
 
+  deleteReservation(id:string){
+    this.reservationService.deleteReservation(id);
+  }
 }
