229ba8d1efa87b3cebdeedef186dd1511329eef8
Anubhaw-Singh <vkt94594@gmail.com>
2025-08-23 17:15:18 +0530

    using Reservation service for CRUD implementation

--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+private reservations : Reservation[] =[];
+
+// CRUD
+
+// Reading all reservations
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+// Reading individual reservation
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id)
+  }
+
+// Creating a Reservation
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation)
+  }
+
+// Deleting a Reservation
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index,1);
+  }
+
+// Updating a Reservation
+  updateReservation(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id = updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+  }
+
 }
